128e157940d1833c1064aada27581dee220b5fdb
shikharsun <shikhar21121999@gmail.com>
2021-05-23 11:51:32 +0530

    project initialization commit

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define pii pair<int,int>
+#define vpii vector<pii>
+
+
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		cout<<"hello world\n";
+	}
+}
